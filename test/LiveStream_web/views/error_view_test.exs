66bcac7f4805aba742809c3590f1b7dc4a6ed1a6
ryanyogan <ryanyogan@gmail.com>
2020-01-26 12:08:40 -0800

    initial commit

---
+++ test/LiveStream_web/views/error_view_test.exs
@@ -0,0 +1,14 @@
+defmodule LiveStreamWeb.ErrorViewTest do
+  use LiveStreamWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.html" do
+    assert render_to_string(LiveStreamWeb.ErrorView, "404.html", []) == "Not Found"
+  end
+
+  test "renders 500.html" do
+    assert render_to_string(LiveStreamWeb.ErrorView, "500.html", []) == "Internal Server Error"
+  end
+end
